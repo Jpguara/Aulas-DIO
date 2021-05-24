a0def08a3411abf64eeb0f73cf1c299437fb773f
Jpguara <joaopedrocarvalho2906@gmail.com>
2021-05-24 10:09:35 -0300

    commit inicial

---
+++ Primeiro_Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
